ff333c5f5cacdbd3727ddc2dae89243641630671
Sarath Kaul <kaul.sarath@gmail.com>
2020-10-20 16:52:47 +0000

    Args Check

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,7 +6,12 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"Hello {args[0]}!");
+            if (args.Length > 0) {
+                Console.WriteLine($"Hello {args[0]}!");
+            }
+            else{
+                Console.WriteLine("Arguments not passed");
+            }
         }
     }
 }
